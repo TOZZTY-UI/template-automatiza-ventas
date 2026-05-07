b25cd694d393be308676313b33e92aea2e35f3ca
TOZZTY <vmoralesarriagada.r@outlook.com>
2026-05-06 21:13:31 -0400

    feat: update footer and header components for improved responsiveness and accessibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+
   },
 });
